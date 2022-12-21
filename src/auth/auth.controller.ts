6b9fc39c95eab6c0f770fd15149e53dccca13162
fcaramez <francisco.caramez.duarte@gmail.com>
2022-12-21 20:23:46 +0000

    adds whitelist and basic routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('/signup')
+  signup() {
+    return '';
+  }
+
+  @Post('/login')
+  login() {
+    return '';
+  }
+
+  @Get('/me')
+  getCurrentUser() {
+    return '';
+  }
+}
